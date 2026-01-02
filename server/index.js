fcd21f145bedb4d26ab0ddefd786596f485c7436
pooja-mandale <devpooja9511@gmail.com>
2026-01-03 00:46:07 +0530

    ndex change

--- server/index.js
+++ server/index.js
@@ -7,8 +7,12 @@ require("dotenv").config({ path: "./.env" })
 
 const app = express()
 app.use(cors({
-    origin: true,
-    credentials: true
+    origin: process.env.NODE_ENV === "production" 
+        ? "https://clothing-project-eight.vercel.app/" 
+        : "http://localhost:5173", 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use(cookieParser())
