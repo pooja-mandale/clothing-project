121a8005cbe8502dbfa8260d81cf7e5823adfd60
pooja-mandale <devpooja9511@gmail.com>
2026-01-04 17:33:26 +0530

    chunks sze increase

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,4 +8,18 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  build: {
+    // 1. Increases the limit to 1600kb to stop the warning
+    chunkSizeWarningLimit: 1600, 
+    rollupOptions: {
+      output: {
+        // 2. Splits node_modules into separate chunks for better loading
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 })
