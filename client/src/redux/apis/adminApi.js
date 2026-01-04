d359f605f26c79a281b79ce049d85050494c76cf
pooja-mandale <devpooja9511@gmail.com>
2026-01-04 16:40:35 +0530

    api update

--- client/src/redux/apis/adminApi.js
+++ client/src/redux/apis/adminApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const adminApi = createApi({
   reducerPath: "adminApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/admin",
+    baseUrl: "https://clothing-project-eight.vercel.app/api/admin",
     credentials: "include", // cookie support
   }),
   tagTypes: ["Admin"],
