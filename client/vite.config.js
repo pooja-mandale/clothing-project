329b14be1fcc0eb21b34f3978b92cdf2da6ac156
pooja-mandale <devpooja9511@gmail.com>
2026-01-04 17:37:20 +0530

    chunks size increase

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,21 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   build: {
-    // 1. Increases the limit to 1600kb to stop the warning
-    chunkSizeWarningLimit: 1600, 
+    chunkSizeWarningLimit: 2000, // Increase limit to 2MB
     rollupOptions: {
       output: {
-        // 2. Splits node_modules into separate chunks for better loading
+        // This forces small helper libraries into one 'vendor' chunk
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            return 'vendor';
           }
         },
       },
